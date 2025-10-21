5f62a8524fce3f209a86d8ab6851eec84582de56
Sara <sthompson2611@gmail.com>
2025-10-21 16:21:24 -0700

    Added js funcitonality to the submit and clear buttons

---
+++ app.js
@@ -0,0 +1,17 @@
+
+//defining form submit and clear buttons
+
+const submitLogin = document.getElementById("login-submit");
+
+submitLogin.addEventListener('click', function(e) {
+            e.preventDefault();
+           alert("Successful Login!")
+            // const username = document.getElementById(username);
+            // console.log(username)
+})
+
+const clearLogin = document.getElementById("clear-login");
+
+clearLogin.addEventListener('click', function(e){
+    
+})
